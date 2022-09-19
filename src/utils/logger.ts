6cbafb94dbf08ae3692dea4f86d86aa734411bc7
tfkhdyt <tfkhdyt@pm.me>
2022-09-19 09:45:09 +0700

    fix(logger): set timezone to jakarta

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,13 +2,15 @@ import WAWebJS from 'whatsapp-web.js';
 
 const printLog = (contact: WAWebJS.Contact, messageBody: string) => {
   // get current timestamp
-  const timeStamp = new Date().toLocaleString('id-ID');
+  const timeStamp = new Date().toLocaleString('id-ID', {
+    timeZone: 'Asia/Jakarta',
+  });
 
   // print log
   console.log(
     `[${timeStamp}] ${contact.name ?? 'Unknown'} (${
       contact.id.user
-    }) berkata: ${messageBody}`
+    }) said: ${messageBody}`
   );
 };
 
