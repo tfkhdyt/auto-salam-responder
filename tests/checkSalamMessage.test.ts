bf0c19b9ae3e4c19dabbd09ff9d00f188e3e43f4
tfkhdyt <tfkhdyt@pm.me>
2022-09-23 08:40:43 +0700

    test: add test for salam message

---
+++ tests/checkSalamMessage.test.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect } from 'vitest';
+
+import checkSalamMessage from '../src/utils/checkSalamMessage';
+
+describe('check salam message testing', () => {
+  it('should returns true', () => {
+    expect(checkSalamMessage('assalamualaikum')).toEqual(true);
+    expect(checkSalamMessage('Assalamualaikum')).toEqual(true);
+    expect(checkSalamMessage("assalamu'alaikum")).toEqual(true);
+    expect(checkSalamMessage("Assalamu'alaikum")).toEqual(true);
+  });
+
+  it('should returns false', () => {
+    expect(checkSalamMessage('ass')).toEqual(false);
+    expect(checkSalamMessage('Ass')).toEqual(false);
+    expect(checkSalamMessage('mikum')).toEqual(false);
+    expect(checkSalamMessage('Mikum')).toEqual(false);
+    expect(checkSalamMessage('assalamualaykum')).toEqual(false);
+    expect(checkSalamMessage('Assalamualaykum')).toEqual(false);
+    expect(checkSalamMessage("assalamu'alaykum")).toEqual(false);
+    expect(checkSalamMessage("Assalamu'alaykum")).toEqual(false);
+  });
+});
